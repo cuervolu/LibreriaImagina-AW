39c66aacd60e06afc7a5de658532b182e419368d
cuervolu <cuervolu@protonmail.com>
2023-05-16 13:05:01 -0400

    Se agrega url de agregar libro a carrito

--- libreria_imagina/urls.py
+++ libreria_imagina/urls.py
@@ -5,6 +5,8 @@ from .templates.auth import urls as auth_urls
 
 urlpatterns = [
     path('', index, name = 'index'),
+    path('agregar_al_carrito/<int:id_libro>/', agregar_al_carrito, name='agregar_al_carrito'),
+    path('carrito', carrito, name = 'carrito'),
 
     # Incluir las URLs de autenticación desde auth.urls
     path('auth/', include(auth_urls, namespace='auth'))
